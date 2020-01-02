62f41052a65129a0545673d9ea77dd9e60f3949f
Shelby England <Shelby.England@knights.ucf.edu>
2020-01-02 17:46:11 -0500

    renamed file

--- server.js
+++ server.js
@@ -13,11 +13,10 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_Controller.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
 app.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
 });
-
